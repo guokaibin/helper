3b2df76c06b22515fed3508010839374e653353d
leo <15570353396@163.com>
2017-08-04 01:03:31 +0800

    完成shiro登录

---
+++ src/main/java/com/helper/realm/Constants.java
@@ -0,0 +1,35 @@
+package com.helper.realm;
+
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.crypto.SecureRandomNumberGenerator;
+import org.apache.shiro.crypto.hash.SimpleHash;
+import org.apache.shiro.util.ByteSource;
+
+public class Constants {
+	public static String VALIDATE_CODE="_VALIDATE_CODE";
+	
+	
+
+	public static void main(String[] args) {  
+		  String algorithmName = "md5";  
+		  String username = "15570353396@163.com";  
+		  String password = "111111";  
+		  String salt1 = username;  
+		  String salt2 = new SecureRandomNumberGenerator().nextBytes().toHex();  
+		  int hashIterations = 3;  
+		  SimpleHash hash = new SimpleHash(algorithmName, password,  salt1 + salt2, hashIterations);  
+		  String encodedPassword = hash.toHex();  
+		  System.out.println(encodedPassword);  
+		  System.out.println(salt2);  
+		  
+		  
+//		   String username1 = "liu"; //用户名及salt1  
+//		    String password1 = "202cb962ac59075b964b07152d234b70"; //加密后的密码  
+//		    String salt21 = "202cb962ac59075b964b07152d234b70";  
+//		SimpleAuthenticationInfo ai =   
+//		        new SimpleAuthenticationInfo(username, password, getName());  
+//		    ai.setCredentialsSalt(ByteSource.Util.bytes(username+salt2)); //盐是用户名+随机数  
+		        
+	} 
+	
+}
